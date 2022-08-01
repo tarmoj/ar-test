b07a2f60e9b809dcc7229c237540d7f9af512a42
tarmo <trmjhnns@gmail.com>
2022-08-01 14:21:57 +0300

    More improvements to ws business, implemented again in planets.html

--- websocket.js
+++ websocket.js
@@ -1,22 +1,30 @@
 // websocket stuff
 
 let websocket = null;
-let time = -1;
 
-// TODO: REWRITE according to websocket-test
+// would be better to rewrite as a class or similar...
+
+function setStatus(status) { // TODO: give the element
+    document.querySelector("#status").innerHTML=status;
+}
 
 function doConnect()
 {
+    if (websocket && websocket.readyState===WebSocket.OPEN) {
+        console.log("Socket seems to be OK and open. Ignoring.");
+        return;
+    }
     websocket = new WebSocket("wss://data.elektron.art");
     websocket.onopen = function(evt) { onOpen(evt) };
     websocket.onclose = function(evt) { onClose(evt) };
     websocket.onmessage = function(evt) { onMessage(evt) };
-    websocket.onerror = function(evt) { onError(evt) };
+    websocket.onerror = function(evt) { onClose(evt) };
 }
 
 function onOpen(evt)
 {
     console.log("connected\n");
+    setStatus("connected");
     // const connectButton = document.getElementById("connectButton");
     // //connectButton.disabled = true;
     // connectButton.innerHTML = "Connected";
@@ -25,50 +33,38 @@ function onOpen(evt)
 
 function onClose(evt)
 {
-    console.log("disconnected\n");
-    // const connectButton = document.getElementById("connectButton");
-    // connectButton.disabled = false;
-    // connectButton.innerHTML = "Connect";
+    console.log("disconnected");
+    setStatus("disconnected");
+    if (typeof(wsClosedCallback)==="function") {
+        wsClosedCallback();
+    }
 }
 
 function onMessage(event)
 {
-    //console.log("response: " + evt.data + '\n');
     const data = JSON.parse(event.data);
-    console.log("Data: ", data);
-    if (data.hasOwnProperty("TT_time")) {
-        console.log("Time is in seconds: ", data.TT_time )
+    //console.log("Data: ", data);
+
+    if (typeof (wsMessageCallback)==="function") {
+        wsMessageCallback(data);
     }
 
-    const mess_array = console.log();
-    // let's assume it is time
-    //console.log(mess_array[0]);
-    let timeString = "xx:xx"
-    if (mess_array[0] == "b") {
-        timeString = mess_array[1].padStart(2, "0") + ":" +  mess_array[2].padStart(2, "0");
-        time = parseInt(mess_array[1])*60 + parseInt(mess_array[2]);
-        console.log(time, timeString);
-
-        if (wsMessageCallback) {
-            wsMessageCallback(time, timeString);
-        }
-    }
 }
+//
+// function onError(evt)
+// {
+//     console.log('error: ' + evt.data + '\n');
+//     websocket.close();
+// }
 
-function onError(evt)
-{
-    console.log('error: ' + evt.data + '\n');
-    websocket.close();
-    // const connectButton = document.getElementById("connectButton");
-    // connectButton.disabled = false;
-    // connectButton.innerHTML = "Connect";
-
-}
-
-
+//
 function doSend(message)
 {
-    console.log("sent: " + message + '\n');
-    websocket.send(message);
+    if (websocket.readyState === WebSocket.OPEN) {
+        websocket.send(message);
+        console.log("sent: " + message);
+    } else {
+        console.log("Socket is not open");
+    }
 }
 
