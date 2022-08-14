f016bc3508a8bdc54d28a69abf4e0b4f9cfceb29
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-08-14 14:28:09 +0300

    cleanup

---
+++ utils.js
@@ -0,0 +1,43 @@
+function seq(count, step = 1, start = 0) {
+  return Array.from({ length: count }).map((_, i) =>
+    typeof step === "function" ? step(i + start) : i * step + start
+  );
+}
+
+function range(from, to, step = 1) {
+  const reversed = to < from;
+  if (reversed) {
+    [to, from] = [from, to];
+  }
+  const length = Math.floor((to - from) / step) + 1;
+  const output = seq(length, step, from);
+  return reversed ? output.reverse() : output;
+}
+
+function deg2rad(deg = 0) {
+  return (deg * Math.PI) / 180;
+}
+
+function rad2deg(rad = 0) {
+  return (rad * 180) / Math.PI;
+}
+
+function pol2car(angle, radius) {
+  return [
+    Math.cos((angle - 90) * (Math.PI / 180)) * radius,
+    Math.sin((angle - 90) * (Math.PI / 180)) * radius,
+  ];
+}
+
+function car2pol(point) {
+  const [x, y] = point;
+  return [Math.atan2(y, x) * (180 / Math.PI), Math.sqrt(x * x + y * y)];
+}
+
+function remap(value, start1, stop1, start2, stop2) {
+  return ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+}
+
+function random(min = 0, max = 1) {
+  return min + Math.random() * (max - min);
+}
